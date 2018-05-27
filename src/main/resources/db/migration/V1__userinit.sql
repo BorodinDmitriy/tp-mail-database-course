9ef60a89fe069874e6e36c7c2d49ec938a936a3f
Borodin Dmitriy <demansen@yandex.ru>
2018-05-27 22:10:13 +0300

    changes

--- src/main/resources/db/migration/V1__userinit.sql
+++ src/main/resources/db/migration/V1__userinit.sql
@@ -1,13 +1,7 @@
 CREATE EXTENSION IF NOT EXISTS CITEXT;
 
--- SET SYNCHRONOUS_COMMIT = 'off';
+SET SYNCHRONOUS_COMMIT = 'off';
 
-DROP TABLE IF EXISTS userprofiles CASCADE;
-DROP TABLE IF EXISTS forums CASCADE;
-DROP TABLE IF EXISTS threads CASCADE;
-DROP TABLE IF EXISTS posts CASCADE;
-DROP TABLE IF EXISTS forums_and_users CASCADE;
-DROP TABLE IF EXISTS votes CASCADE;
 
 CREATE TABLE IF NOT EXISTS userprofiles (
   id       SERIAL PRIMARY KEY,
@@ -16,6 +10,11 @@ CREATE TABLE IF NOT EXISTS userprofiles (
   fullname TEXT DEFAULT NULL,
   nickname CITEXT COLLATE ucs_basic UNIQUE
 );
+
+create index if not exists user_name_idx on userprofiles(nickname);
+
+
+----------------------------------------------------------------------------------
 
 CREATE TABLE IF NOT EXISTS forums (
   id      SERIAL PRIMARY KEY,
@@ -26,6 +25,14 @@ CREATE TABLE IF NOT EXISTS forums (
   posts   INTEGER DEFAULT 0,
   threads INTEGER DEFAULT 0
 );
+
+create index if not exists forums_slug_idx on forums(slug);
+
+CREATE INDEX IF NOT EXISTS forums_userprofiles_for_id_idx
+ON forums (owner_id);
+
+-------------------------------------------------------------------------------------
+
 
 CREATE TABLE IF NOT EXISTS threads (
   id SERIAL PRIMARY KEY,
@@ -39,6 +46,14 @@ CREATE TABLE IF NOT EXISTS threads (
   votes    INTEGER     DEFAULT 0,
   slug     CITEXT UNIQUE
 );
+
+
+CREATE INDEX IF NOT EXISTS threads_user_id_idx
+  ON threads (author_id);
+CREATE INDEX IF NOT EXISTS threads_forum_id_idx
+ON threads (forum_id);
+
+--------------------------------------------------------------------------------------
 
 
 CREATE TABLE IF NOT EXISTS posts (
@@ -56,19 +71,107 @@ CREATE TABLE IF NOT EXISTS posts (
   path_to_post INTEGER []
 );
 
+
+create index if not exists post_thread on posts(thread_id);
+create index if not exists post_thread_post on posts(thread_id, id);
+create index if not exists post_root ON posts(id_of_root);
+CREATE INDEX IF NOT EXISTS posts_multi_idx ON posts (thread_id, parent_id);
+
 CREATE TABLE IF NOT EXISTS forums_and_users (
   user_id INTEGER REFERENCES userprofiles (id) ON DELETE CASCADE NOT NULL,
   forum_id INTEGER REFERENCES forums (id) ON DELETE CASCADE NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS forum_users_user_id_idx
+  ON forums_and_users (user_id);
+CREATE INDEX IF NOT EXISTS forum_users_forum_id_idx
+ON forums_and_users (forum_id);
+
+
 
 
 CREATE TABLE IF NOT EXISTS votes (
   owner_id INTEGER REFERENCES userprofiles (id) ON DELETE CASCADE,
   thread_id  INTEGER REFERENCES threads (id) ON DELETE CASCADE,
   vote INTEGER DEFAULT 0,
-  CONSTRAINT owner_per_thread UNIQUE (owner_id, thread_id)
+  CONSTRAINT one_owner_thread_pair UNIQUE (owner_id, thread_id)
 );
 
+create index if not exists thread_vote_user on votes(owner_id, thread_id);
+create index if not exists thread_vote on votes(thread_id);
+
+------------------- TRIGGER FOR UPDATE threads ---------------
+CREATE OR REPLACE FUNCTION insert_threads_func() RETURNS TRIGGER AS
+$insert_threads_trigger$
+  BEGIN
+      UPDATE threads SET author_name = (SELECT nickname FROM userprofiles WHERE id = NEW.author_id),
+                      forum_slug = (SELECT slug FROM forums WHERE id = NEW.forum_id)
+      WHERE id = NEW.id;
+      UPDATE forums SET threads = threads + 1 WHERE id = NEW.forum_id;
+       INSERT INTO forums_and_users(user_id, forum_id) VALUES(NEW.author_id, NEW.forum_id);
+    RETURN NULL;
+  END;
+$insert_threads_trigger$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS insert_threads_trigger ON threads;
+CREATE TRIGGER insert_threads_trigger AFTER INSERT ON threads
+  FOR EACH ROW EXECUTE PROCEDURE insert_threads_func();
+
+--------------------- TRIGGER FOR UPDAT..E forums ------------------------
+CREATE OR REPLACE FUNCTION insert_forum_func() RETURNS TRIGGER AS
+$insert_forums_trigger$
+  BEGIN
+    UPDATE forums SET owner_name = (SELECT nickname FROM userprofiles WHERE id = NEW.owner_id)
+      WHERE id = NEW.id;
+    RETURN NULL;
+  END;
+$insert_forums_trigger$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS insert_forums_trigger ON forums;
+CREATE TRIGGER insert_forums_trigger AFTER INSERT ON forums
+  FOR EACH ROW EXECUTE PROCEDURE insert_forum_func();
+---------------------------------------------------------------------
+
+CREATE OR REPLACE FUNCTION insert_posts_func() RETURNS TRIGGER AS
+$insert_posts_trigger$
+  DECLARE
+    arr INTEGER[];
+    root INTEGER;
+  BEGIN
+      IF NEW.parent_id = 0 THEN
+       SELECT array_append(NULL, NEW.id) into arr;
+       root = NEW.id;
+      ELSE
+        SELECT array_append((SELECT path_to_post from posts WHERE id = NEW.parent_id), NEW.id) into arr;
+        root = arr[1];
+      END IF;
+      NEW.path_to_post = arr;
+      NEW.id_of_root = root;
+      UPDATE forums set posts = posts + 1 WHERE id = NEW.forum_id;
+      INSERT INTO forums_and_users(user_id, forum_id) VALUES(NEW.author_id, NEW.forum_id);
+    RETURN NEW;
+  END;
+$insert_posts_trigger$ LANGUAGE plpgsql;
+--
+
+DROP TRIGGER IF EXISTS insert_posts_trigger ON posts;
+CREATE TRIGGER insert_posts_trigger BEFORE INSERT ON posts
+  FOR EACH ROW EXECUTE PROCEDURE insert_posts_func();
+
+---------------------- vote -----------------------------------------
+CREATE OR REPLACE FUNCTION create_or_update_vote(u_id INTEGER, t_id INTEGER, v INTEGER)
+  RETURNS VOID AS '
+BEGIN
+  INSERT INTO votes (owner_id, thread_id, vote) VALUES (u_id, t_id, v)
+  ON CONFLICT (owner_id, thread_id)
+    DO UPDATE SET vote = v;
+  UPDATE threads
+  SET votes = (SELECT SUM(vote)
+               FROM votes
+               WHERE thread_id = t_id)
+  WHERE id = t_id;
+END;'
+LANGUAGE plpgsql;
 
 
 
